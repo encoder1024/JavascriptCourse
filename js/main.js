92cc70d11020bc6f742dee1b594dc00d95c41c01
Andres Ferrer <48453885+encoder1024@users.noreply.github.com>
2022-08-18 22:41:44 -0300

    ajustes de código en la condición de validar entre 1 y 100

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@ console.log(luckyNumber);
 
 do{
   let guest = parseInt(prompt("Adivina el número entre 1 y 100..."));
-  if (guest == luckyNumber) {
-    console.log("Adivinaste!!! Eres libre!!!");
-    continuar = false;
-  } else if( guest > luckyNumber){
-    console.log("Tú número es alto");
+  if( guest > 0 && guest <= 100){
+    if (guest == luckyNumber) {
+      console.log("Adivinaste!!! Eres libre!!!");
+      continuar = false;
+    } else if( guest > luckyNumber){
+      console.log("Tú número es alto");
+    } else {
+      console.log("Tú número es bajo");
+    }
   } else {
-    console.log("Tú número es bajo");
+    console.log("ingresa un número entre 1 y 100 por favor... será más fácil...");
   }
-} while (continuar);
+} while(continuar);
