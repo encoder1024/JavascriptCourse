d3aefc373c73ca52fc8a0b8a369249dd93c4c0ae
Andres Ferrer <48453885+encoder1024@users.noreply.github.com>
2022-08-18 23:17:31 -0300

    codigo mejorado y pruebo @ts-check

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
+// @ts-check
 let continuar = true;
 let luckyNumber = Math.floor((Math.random() * 100) + 1);
 console.log(luckyNumber);
 
 do{
-  let guest = parseInt(prompt("Adivina el número entre 1 y 100..."));
+  let guest = parseInt(prompt("Adivina el número entre 1 y 100...") || "0");
   if( guest > 0 && guest <= 100){
     if (guest == luckyNumber) {
       console.log("Adivinaste!!! Eres libre!!!");
       continuar = false;
     } else if( guest > luckyNumber){
-      console.log("Tú número es alto");
+      console.log("Tú número " + guest + " es alto");
     } else {
-      console.log("Tú número es bajo");
+      console.log("Tú número " + guest + " es bajo");
     }
   } else {
     console.log("ingresa un número entre 1 y 100 por favor... será más fácil...");
