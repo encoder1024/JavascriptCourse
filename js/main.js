5cd66bad086ed8de0c8d9c4f99bf1a7d4b43520f
Andres Ferrer <48453885+encoder1024@users.noreply.github.com>
2022-08-28 21:19:41 -0300

    agrego una función

--- js/main.js
+++ js/main.js
@@ -1,34 +1,46 @@
 let puntosCompu = 0;
 let puntosUsuario = 0;
+let nombreUsuario = "";
+
+function solicitarNombreUsuario() {
+  do{
+    let userName = prompt("Ingresa tu nombre antes de competir:");
+  } while (userName != null && userName.lenght() > 3)
+
+  alert("Bienvenido " + userName + "!!! .... te voy a ganar! ... pulsa aceptar por favor...");
+  nombreUsuario = userName;
+}
+
+solicitarNombreUsuario();
 
 for (let i=0; i < 5; i++) {
   let luckyNumber = Math.floor(Math.random() * 3);
 
   if (luckyNumber == 0) {
-      luckyNumber = 'piedra';
+      luckyNumber = "piedra";
   } else if (luckyNumber == 1) {
-      luckyNumber = 'papel';
+      luckyNumber = "papel";
   } else {
-      luckyNumber = 'tijera';
+      luckyNumber = "tijera";
   }
 
-  let jugador = prompt('piedra papel o tijera?')
+  let jugador = prompt("piedra papel o tijera?")
 
-  if (jugador !== 'piedra' && jugador !== 'papel' && jugador !== 'tijera') {
-      alert('Volve a elegir correctamente!')
-  } else if (jugador == 'piedra' && luckyNumber == 'tijera') {
-      alert('Felicitaciones ganaste! elegiste ' + jugador + ' y ' + 'la computadora eligio ' + luckyNumber);
+  if (jugador !== "piedra" && jugador !== "papel" && jugador !== "tijera") {
+      alert("Volve a elegir correctamente!")
+  } else if (jugador == "piedra" && luckyNumber == "tijera") {
+      alert("Felicitaciones "+ nombreUsuario +" ganaste! elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
       puntosUsuario++;
-  } else if (jugador == 'tijera' && luckyNumber == 'papel') {
-      alert('Felicitaciones ganaste! elegiste ' + jugador + ' y ' + 'la computadora eligio ' + luckyNumber);
+  } else if (jugador == "tijera" && luckyNumber == "papel") {
+      alert("Felicitaciones "+ nombreUsuario +" ganaste! elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
       puntosUsuario++;
-  } else if (jugador == 'papel' && luckyNumber == 'piedra') {
-      alert('Felicitaciones ganaste! hiciste ' + jugador + ' y ' + 'la computadora eligio ' + luckyNumber);
+  } else if (jugador == "papel" && luckyNumber == "piedra") {
+      alert("Felicitaciones "+ nombreUsuario +" ganaste! hiciste " + jugador + " y " + "la computadora eligio " + luckyNumber);
       puntosUsuario++;
   } else if (jugador == luckyNumber) {
-      alert('Empate. Elegiste ' + jugador + ' y ' + 'la computadora eligio ' + luckyNumber);
+      alert("Empate. Elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
   } else {
-      alert('perdiste :(. Elegiste ' + jugador + ' y ' + 'la computadora eligio ' + luckyNumber);
+      alert("perdiste :(. Elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
       puntosCompu++;
   }
 }
@@ -36,7 +48,7 @@ for (let i=0; i < 5; i++) {
 if(puntosCompu > puntosUsuario) {
   alert("("+puntosCompu+" a "+ puntosUsuario +") -> Te ha ganado una vez más una simple máquina que solo suma... pero suma muy muy rápido!!!");
 } else if( puntosCompu < puntosUsuario) {
-  alert("Felicitaciones!!! eres un maestro del kunfu!!! ... ganaste por " + (puntosUsuario-puntosCompu));
+  alert("Felicitaciones "+ nombreUsuario +"!!! eres un maestro del kunfu!!! ... ganaste por " + (puntosUsuario-puntosCompu));
 } else {
   alert("("+puntosCompu+" a "+ puntosUsuario +") ->Empataste con una máquina... mal de muchos, consuelo de tontos...vuelve a intentar!!!");
 }
