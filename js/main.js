246b91b2502aff60f25d83ce4d74d3f8d4477359
Andres Ferrer <48453885+encoder1024@users.noreply.github.com>
2022-09-04 16:51:14 -0300

    agrego el array para los resultados y uso algunos metodos para mostrar estadisticas al final del juego

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 let puntosCompu = 0;
 let puntosUsuario = 0;
-let nombreUsuario = "";
+
+let opciones = ["piedra","papel","tijera"];
+
+const getLastArrItem = (arr) => { 
+  let lastItem=arr[arr.length-1];
+  return lastItem;  
+}  
 
 class Usuario{
+  resultados = [];
+
   constructor (nombre, puntaje){
     this.nombre = nombre;
     this.puntaje = puntaje;
@@ -14,7 +22,7 @@ class Usuario{
       userName = prompt("Ingresa tu nombre antes de competir:");
     } while (userName == null || !isNaN(userName))
   
-    alert("Bienvenido " + userName + "!!! .... te voy a ganar! ... pulsa aceptar, es al mejor de 5...");
+    alert("Bienvenido " + userName + "!!! .... te voy a ganar! ... pulsa aceptar para iniciar el juego!");
     this.nombre = userName;
     return this.nombre;
   }
@@ -26,55 +34,131 @@ class Usuario{
   puntajeBaja(step){
     this.puntaje = this.puntaje - step;
   }
+
+  continuar(){
+    let seguimos = true;
+    let incompleto = true;
+    do{
+      let opcion = parseInt(prompt("1-Continuar o 2-Terminar", 1), 10);
+      if (isNaN(opcion) || opcion > 2 || opcion < 1) {
+        alert("Volver a elegir correctamente!");
+      } else {
+        if(opcion == 2){
+          seguimos = false;
+        }
+        incompleto = false;
+      }
+    } while (incompleto);
+    return seguimos;
+  }
+
+  contarIgualdades(resultados){
+    let cuentaEmpates = 0;
+    for (const result of resultados) {
+      const auxiliar = result.split("-");
+      if(auxiliar[1] == auxiliar[2]){
+        cuentaEmpates++;
+      }
+    }
+    return cuentaEmpates;
+  }
+
+  contarEmpates(resultados){
+    let cuentaEmpates = 0;
+    let auxiliar = getLastArrItem(resultados).split("-");
+    cuentaEmpates = auxiliar[0]-auxiliar[1]-auxiliar[2];
+    return cuentaEmpates;
+  }
 }
 
 usuarioPrincipal = new Usuario("", 0);
 
 usuarioPrincipal.solicitarNombreUsuario();
 
-for (let i=0; i < 5; i++) {
+let i = 1;
+
+do {
   let luckyNumber = Math.floor(Math.random() * 3);
+  let opcionCompu = "";
+  
 
-  if (luckyNumber == 0) {
-      luckyNumber = "piedra";
-  } else if (luckyNumber == 1) {
-      luckyNumber = "papel";
+  opcionCompu = opciones[luckyNumber];
+
+  let opcionJugadorNum = parseInt(prompt("1-"+opciones[0]+", 2-"+opciones[1]+" o 3-"+opciones[2]+"?", 1),10);
+  let opcionJugador = "";
+
+  console.log(opcionJugadorNum);
+
+  if (isNaN(opcionJugadorNum) || opcionJugadorNum > 3 || opcionJugadorNum < 1) {
+    alert("Volver a elegir correctamente!, este intento se anula para ambos.");
+    console.log(opcionJugadorNum);
   } else {
-      luckyNumber = "tijera";
+    console.log(opcionJugadorNum);
+    opcionJugador = opciones[opcionJugadorNum-1];
+    if (opcionJugador == opciones[0] && opcionCompu == opciones[2]) {
+        alert("Felicitaciones "+ usuarioPrincipal.nombre +" ganaste! elegiste " + 
+        opcionJugador + " y " + "la computadora eligio " + opcionCompu);
+        usuarioPrincipal.puntajeSube(1);
+    } else if (opcionJugador == opciones[2] && opcionCompu == opciones[1]) {
+        alert("Felicitaciones "+ usuarioPrincipal.nombre +" ganaste! elegiste " + 
+        opcionJugador + " y " + "la computadora eligio " + opcionCompu);
+        usuarioPrincipal.puntajeSube(1);
+    } else if (opcionJugador == opciones[1] && opcionCompu == opciones[0]) {
+        alert("Felicitaciones "+ usuarioPrincipal.nombre +" ganaste! hiciste " + 
+        opcionJugador + " y " + "la computadora eligio " + opcionCompu);
+        usuarioPrincipal.puntajeSube(1);
+    } else if (opcionJugador == opcionCompu) {
+        alert(usuarioPrincipal.nombre +" empataste. Elegiste " + opcionJugador + " y " +
+        "la computadora eligio " + opcionCompu);
+        console.log("*empate*")
+    } else {
+        alert(usuarioPrincipal.nombre +" perdiste. Elegiste " + opcionJugador + " y " +
+        "la computadora eligio " + opcionCompu);
+        puntosCompu++;
+    }
   }
+  
+  usuarioPrincipal.resultados.push(i +"-"+ puntosCompu +"-"+ usuarioPrincipal.puntaje);
 
-  let jugador = prompt("piedra, papel o tijera?")
+  console.log(i +"-"+ puntosCompu +"-"+ usuarioPrincipal.puntaje);
 
-  if (jugador !== "piedra" && jugador !== "papel" && jugador !== "tijera") {
-      alert("Volver a elegir correctamente!, este intento se anula para ambos.")
-  } else if (jugador == "piedra" && luckyNumber == "tijera") {
-      alert("Felicitaciones "+ usuarioPrincipal.nombre +" ganaste! elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
-      puntosUsuario++;
-  } else if (jugador == "tijera" && luckyNumber == "papel") {
-      alert("Felicitaciones "+ usuarioPrincipal.nombre +" ganaste! elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
-      puntosUsuario++;
-  } else if (jugador == "papel" && luckyNumber == "piedra") {
-      alert("Felicitaciones "+ usuarioPrincipal.nombre +" ganaste! hiciste " + jugador + " y " + "la computadora eligio " + luckyNumber);
-      puntosUsuario++;
-  } else if (jugador == luckyNumber) {
-      alert(usuarioPrincipal.nombre +" empataste. Elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
-      console.log("*empate*")
-  } else {
-      alert(usuarioPrincipal.nombre +" perdiste. Elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
-      puntosCompu++;
-  }
+  i++;
 
-  console.log(i +"-"+ puntosCompu +"-"+ puntosUsuario);
+} while(usuarioPrincipal.continuar())
 
-}
-
-if(puntosCompu > puntosUsuario) {
-  alert("("+puntosCompu+" a "+ puntosUsuario +") -> "+ usuarioPrincipal.nombre +" te ha ganado una vez más una simple máquina que solo suma... pero suma muy muy rápido!!!");
-} else if( puntosCompu < puntosUsuario) {
-  alert("Felicitaciones "+ usuarioPrincipal.nombre +"!!! eres un maestro del kunfu!!! ... ganaste por " + (puntosUsuario-puntosCompu));
+if(puntosCompu > usuarioPrincipal.puntaje) {
+  alert("("+puntosCompu+" a "+ usuarioPrincipal.puntaje +") -> "+ usuarioPrincipal.nombre +
+  " te ha ganado una vez más una simple máquina que solo suma... pero suma muy muy rápido!!!"+
+  "\n Mira el detalle: (intento-puntosCompu-puntosUsuario)\n" + usuarioPrincipal.resultados +"\n"+
+  "Igualdades en el puntaje = " + usuarioPrincipal.contarIgualdades(usuarioPrincipal.resultados) +
+  " durante los " + (i-1) + " intentos." + "\n"+ 
+  "Empates = "+(usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados))+ "\n"+
+  "Empates(%) = "+((usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados))/(i-1)*100).toFixed(2)+" %");
+} else if( puntosCompu < usuarioPrincipal.puntaje) {
+    alert("Felicitaciones "+ usuarioPrincipal.nombre +"!!! eres un maestro del kunfu!!! ... ganaste por " +
+    (usuarioPrincipal.puntaje-puntosCompu)+
+    "\n Mira el detalle: (intento-puntosCompu-puntosUsuario)\n" + usuarioPrincipal.resultados +"\n"+
+    "Igualdades en el puntaje = " + usuarioPrincipal.contarIgualdades(usuarioPrincipal.resultados) +
+    " durante los " + (i-1) + " intentos." + "\n"+ 
+    "Empates = "+(usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados))+ "\n"+
+    "Empates(%) = "+((usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados))/(i-1)*100).toFixed(2)+" %");
 } else {
-  alert("("+puntosCompu+" a "+ puntosUsuario +") -> "+ usuarioPrincipal.nombre +" empataste con una máquina... mal de muchos, consuelo de tontos...vuelve a intentar!!!");
+    alert("("+puntosCompu+" a "+ usuarioPrincipal.puntaje +") -> "+ usuarioPrincipal.nombre + 
+    " empataste con una máquina... mal de muchos, consuelo de tontos...vuelve a intentar!!!" +
+    "\n Mira el detalle: (intento-puntosCompu-puntosUsuario)\n" + usuarioPrincipal.resultados +"\n"+
+    "Igualdades en el puntaje = " + usuarioPrincipal.contarIgualdades(usuarioPrincipal.resultados) +
+    " durante los " + (i-1) + " intentos." + "\n"+ 
+    "Empates = "+(usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados))+ "\n"+
+    "Empates(%) = "+((usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados))/(i-1)*100).toFixed(2)+" %");
 }
+
+console.log(usuarioPrincipal.resultados);
+
+console.log("Igualdades en el puntaje = " + usuarioPrincipal.contarIgualdades(usuarioPrincipal.resultados) +
+" durante los " + (i-1) + " intentos.");
+
+console.log("Empates = "+(usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados)));
+console.log("Empates(%) = "+((usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados))/(i-1)*100).toFixed(2)+" %");
 
 console.log("final del código");
 
