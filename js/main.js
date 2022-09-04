bf38a4918e4a42ae57f55ad7a8cf9a77a05c3a1a
Andres Ferrer <48453885+encoder1024@users.noreply.github.com>
2022-09-04 13:26:32 -0300

    agrego el objeto para el usuario

--- js/main.js
+++ js/main.js
@@ -2,22 +2,38 @@ let puntosCompu = 0;
 let puntosUsuario = 0;
 let nombreUsuario = "";
 
-function solicitarNombreUsuario() {
-  let userName = "";
-  do{
-    userName = prompt("Ingresa tu nombre antes de competir:");
-  } while (userName == null || !isNaN(userName))
+class Usuario{
+  constructor (nombre, puntaje){
+    this.nombre = nombre;
+    this.puntaje = puntaje;
+  }
 
-  alert("Bienvenido " + userName + "!!! .... te voy a ganar! ... pulsa aceptar, es al mejor de 5...");
-  nombreUsuario = userName;
+  solicitarNombreUsuario() {
+    let userName = "";
+    do{
+      userName = prompt("Ingresa tu nombre antes de competir:");
+    } while (userName == null || !isNaN(userName))
+  
+    alert("Bienvenido " + userName + "!!! .... te voy a ganar! ... pulsa aceptar, es al mejor de 5...");
+    this.nombre = userName;
+    return this.nombre;
+  }
+
+  puntajeSube(step){
+    this.puntaje = this.puntaje + step;
+  }
+
+  puntajeBaja(step){
+    this.puntaje = this.puntaje - step;
+  }
 }
 
-solicitarNombreUsuario();
+usuarioPrincipal = new Usuario("", 0);
+
+usuarioPrincipal.solicitarNombreUsuario();
 
 for (let i=0; i < 5; i++) {
   let luckyNumber = Math.floor(Math.random() * 3);
-
-  
 
   if (luckyNumber == 0) {
       luckyNumber = "piedra";
@@ -32,32 +48,34 @@ for (let i=0; i < 5; i++) {
   if (jugador !== "piedra" && jugador !== "papel" && jugador !== "tijera") {
       alert("Volver a elegir correctamente!, este intento se anula para ambos.")
   } else if (jugador == "piedra" && luckyNumber == "tijera") {
-      alert("Felicitaciones "+ nombreUsuario +" ganaste! elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
+      alert("Felicitaciones "+ usuarioPrincipal.nombre +" ganaste! elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
       puntosUsuario++;
   } else if (jugador == "tijera" && luckyNumber == "papel") {
-      alert("Felicitaciones "+ nombreUsuario +" ganaste! elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
+      alert("Felicitaciones "+ usuarioPrincipal.nombre +" ganaste! elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
       puntosUsuario++;
   } else if (jugador == "papel" && luckyNumber == "piedra") {
-      alert("Felicitaciones "+ nombreUsuario +" ganaste! hiciste " + jugador + " y " + "la computadora eligio " + luckyNumber);
+      alert("Felicitaciones "+ usuarioPrincipal.nombre +" ganaste! hiciste " + jugador + " y " + "la computadora eligio " + luckyNumber);
       puntosUsuario++;
   } else if (jugador == luckyNumber) {
-      alert("Empate. Elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
+      alert(usuarioPrincipal.nombre +" empataste. Elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
       console.log("*empate*")
   } else {
-      alert("perdiste :(. Elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
+      alert(usuarioPrincipal.nombre +" perdiste. Elegiste " + jugador + " y " + "la computadora eligio " + luckyNumber);
       puntosCompu++;
   }
 
   console.log(i +"-"+ puntosCompu +"-"+ puntosUsuario);
-  
+
 }
 
 if(puntosCompu > puntosUsuario) {
-  alert("("+puntosCompu+" a "+ puntosUsuario +") -> Te ha ganado una vez más una simple máquina que solo suma... pero suma muy muy rápido!!!");
+  alert("("+puntosCompu+" a "+ puntosUsuario +") -> "+ usuarioPrincipal.nombre +" te ha ganado una vez más una simple máquina que solo suma... pero suma muy muy rápido!!!");
 } else if( puntosCompu < puntosUsuario) {
-  alert("Felicitaciones "+ nombreUsuario +"!!! eres un maestro del kunfu!!! ... ganaste por " + (puntosUsuario-puntosCompu));
+  alert("Felicitaciones "+ usuarioPrincipal.nombre +"!!! eres un maestro del kunfu!!! ... ganaste por " + (puntosUsuario-puntosCompu));
 } else {
-  alert("("+puntosCompu+" a "+ puntosUsuario +") ->Empataste con una máquina... mal de muchos, consuelo de tontos...vuelve a intentar!!!");
+  alert("("+puntosCompu+" a "+ puntosUsuario +") -> "+ usuarioPrincipal.nombre +" empataste con una máquina... mal de muchos, consuelo de tontos...vuelve a intentar!!!");
 }
+
+console.log("final del código");
 
 
