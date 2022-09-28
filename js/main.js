ada5aa624e1e1958d2e5a3064717b3308b216fe1
Andres Ferrer <48453885+encoder1024@users.noreply.github.com>
2022-09-28 11:48:58 -0300

    cagregando una libreria para gráficos

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ let i = 1;
 
 let opciones = ["piedra","papel","tijera"];
 
+const ctx = document.getElementById('myChart').getContext('2d');
+
 const
   reset = document.getElementById("resetGeneral"),
   terminar = document.getElementById("terminar"),
@@ -32,6 +34,12 @@ reset.onclick = () => {
   usuarioPrincipal.resultados.length = 0;
   usuarioPrincipal.puntaje = 0;
   puntosCompu = 0;
+  // JS - Destroy exiting Chart Instance to reuse <canvas> element
+  let chartStatus = Chart.getChart("myChart"); // <canvas> id
+  if (chartStatus != undefined) {
+    chartStatus.destroy();
+  }
+  //-- End of chart destroy  
 }
 
 terminar.onclick = () => {
@@ -225,6 +233,35 @@ function jugada(opcionJugadorNum) {
           "Empates = "+(usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados))+ "\n"+
           "Empates(%) = "+((usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados))/(i-1)*100).toFixed(2)+" %";
       }
+      
+      const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Ganados', 'Perdidos', 'Empates'],
+            datasets: [{
+                label: '# of Intents',
+                data: [usuarioPrincipal.puntaje, puntosCompu, usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados)],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
   }
   
   console.log(usuarioPrincipal.resultados);
@@ -236,6 +273,9 @@ function jugada(opcionJugadorNum) {
   console.log("Empates(%) = "+((usuarioPrincipal.contarEmpates(usuarioPrincipal.resultados))/(i-1)*100).toFixed(2)+" %");
   
   console.log("final del código");
+
+
+
 }
 
 
